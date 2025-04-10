f16dd5658a4ade1a6a008cca14c19e2e3a2c023d
ChenKaiLiuG <141424456+ChenKaiLiuG@users.noreply.github.com>
2025-04-10 11:20:53 +0800

    Update myHeal.c

--- hw1/myHeal.c
+++ hw1/myHeal.c
@@ -3,126 +3,76 @@
 #include <string.h>
 #include <stdint.h>
 #include <errno.h>
-#include <sys/stat.h>
-#include <fcntl.h> 
-#include <unistd.h> 
 
-int file_exists(const char *filename) {
-    return (access(filename, F_OK) == 0);
-}
-
-uintmax_t file_size(const char *filename) {
-    struct stat stat_buf;
-    if (stat(filename, &stat_buf) == 0) 
-        return (uintmax_t)stat_buf.st_size;
-        
-    return (uintmax_t)-1;
-}
+#define MAX_CHUNK_NAME_LENGTH 64
 
 int main(int argc, char *argv[]) {
-    if (argc != 5) 
-    {
+    if (argc != 5) {
         fprintf(stderr, "usage: %s <destination> <prefix> <chunk size (K)> <number of chunks>\n", argv[0]);
         return 1;
     }
 
     const char *destination_filename = argv[1];
     const char *prefix = argv[2];
-    long long expected_chunk_size_kb;
-    long long num_chunks;
-    char *endptr;
+    long long chunk_size_kb = strtoll(argv[3], NULL, 10); 
+    long long num_chunks = strtoll(argv[4], NULL, 10);   
 
-    expected_chunk_size_kb = strtoll(argv[3], &endptr, 10);
-    if (*endptr != '\0' || expected_chunk_size_kb <= 0) 
-    {
-        fprintf(stderr, "Error: Invalid chunk size provided.\n");
+    if (chunk_size_kb <= 0 || num_chunks <= 0) {
+        fprintf(stderr, "Error: Invalid chunk size or number of chunks.\n");
         return 1;
     }
 
-    num_chunks = strtoll(argv[4], &endptr, 10);
-    if (*endptr != '\0' || num_chunks <= 0) 
-    {
-        fprintf(stderr, "Error: Invalid number of chunks provided.\n");
+    long long chunk_size_bytes = chunk_size_kb * 1024; 
+    
+    FILE *destination_file = fopen(destination_filename, "wb");
+    if (!destination_file) {
+        fprintf(stderr, "Error: Could not open destination file: %s\n", destination_filename);
         return 1;
     }
 
-    long long expected_chunk_size_bytes = expected_chunk_size_kb * 1024;
-    int destination_fd = open(destination_filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (destination_fd == -1) 
-    {
-        fprintf(stderr, "Error: Could not open/create destination file: %s (%s)\n", destination_filename, strerror(errno));
-        return 1;
-    }
-
-    unsigned char *buffer = (unsigned char *)malloc(expected_chunk_size_bytes);
-    if (!buffer) 
-    {
+    unsigned char *buffer = (unsigned char *)malloc(chunk_size_bytes);
+    if (!buffer) {
         fprintf(stderr, "Error: Memory allocation failed.\n");
-        close(destination_fd);
-        remove(destination_filename);
+        fclose(destination_file);
         return 1;
     }
 
     int missing_chunk = 0;
     int incorrect_chunk_size = 0;
 
-    for (long long i = 0; i < num_chunks; ++i) 
-    {
-        char chunk_filename[512]; 
+    for (long long i = 0; i < num_chunks; ++i) {
+        char chunk_filename[MAX_CHUNK_NAME_LENGTH];
         snprintf(chunk_filename, sizeof(chunk_filename), "%s.%032lld", prefix, i);
 
         printf("putting %s\n", chunk_filename);
-        int chunk_fd = open(chunk_filename, O_RDONLY);
-        if (chunk_fd != -1) 
-        {
-            ssize_t bytes_read = read(chunk_fd, buffer, expected_chunk_size_bytes);
-            if (bytes_read > 0) 
-            {
-                ssize_t bytes_written = write(destination_fd, buffer, bytes_read);
-                if (bytes_written != bytes_read) 
-                {
-                    fprintf(stderr, "Error writing to destination file from chunk: %s (%s)\n", chunk_filename, strerror(errno));
-                    close(chunk_fd);
-                    close(destination_fd);
-                    free(buffer);
-                    remove(destination_filename);
-                    return 1;
-                }
-            } 
-            else if (bytes_read == -1) 
-            {
-                fprintf(stderr, "Error reading chunk file: %s (%s)\n", chunk_filename, strerror(errno));
-                missing_chunk = 1;
-            }
-            close(chunk_fd);
 
-            if (i < num_chunks - 1 && bytes_read != expected_chunk_size_bytes) 
-            {
-                fprintf(stderr, "Warning: Incorrect chunk size for file: %s. Expected: %lld, Actual: %zd\n", chunk_filename, expected_chunk_size_bytes, bytes_read);
+        FILE *chunk_file = fopen(chunk_filename, "rb");
+        if (chunk_file) {
+            size_t bytes_read = fread(buffer, 1, chunk_size_bytes, chunk_file);
+            fwrite(buffer, 1, bytes_read, destination_file);
+            fclose(chunk_file); 
+
+            if (i < num_chunks - 1 && (long long)bytes_read != chunk_size_bytes) {
+                fprintf(stderr, "Warning: Incorrect chunk size for file: %s\n", chunk_filename);
                 incorrect_chunk_size = 1;
             }
-        } 
-        else 
-        {
-            fprintf(stderr, "Error: Could not open chunk file: %s (%s)\n", chunk_filename, strerror(errno));
+        } else {
+            fprintf(stderr, "Error: Could not open chunk file: %s\n", chunk_filename);
             missing_chunk = 1;
         }
     }
 
-    close(destination_fd);
-    free(buffer);
+    fclose(destination_file);
+    free(buffer);             
 
-    if (missing_chunk) 
-    {
-        fprintf(stderr, "Error: One or more chunk files are missing. Reassembly failed.\n");
-        remove(destination_filename);
+    if (missing_chunk) {
+        fprintf(stderr, "Error: One or more chunk files are missing.\n");
+        remove(destination_filename); 
         return 1;
     }
 
-    if (incorrect_chunk_size) 
-    {
-        fprintf(stderr, "Warning: One or more chunk files have an incorrect size. The reassembled file might be incomplete or corrupted.\n");
-        return 1;
+    if (incorrect_chunk_size) {
+        fprintf(stderr, "Warning: One or more chunk files have incorrect size.\n");
     }
 
     printf("done... [%lld] chunks produced for %s\n", num_chunks, destination_filename);
